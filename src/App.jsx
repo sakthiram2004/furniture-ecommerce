2c5cef5571ad7eac36b11314e13e70316c2d4a5c
sakthiram2004 <your.email@example.com>
2025-09-25 09:13:54 +0530

    app jsx changed

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,21 @@
-import {React,useState} from 'react'
+import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+// Components
 import NaveBar from './components/navebar/NaveBar'
 import NavCategorySlider from './components/CategorySlider/CategorySlider'
 import Banner from './components/banner/Banner'
 import ShowProducts from './components/show-products/ShowProducts'
 import Footer from './components/footer/Footer'
 import ProductPage from './components/product-page/ProductPage'
-import ProductList from './components/productlist/ProductList '
+import ProductList from './components/productlist/ProductList'
 import ProfilePage from './components/profile/ProfilePage'
 import FavoritesPage from './components/favor/FavoritesPage'
 import CartPage from './components/cart/CartPage'
 import CategorySlider from './components/CategorySlider/CategorySlider'
+
+// Pages
 import Home from './pages/Home'
-import { BrowserRouter, Route, Router, Routes } from 'react-router-dom'
 import Products from './pages/Products'
 import ProductDetails from './pages/ProductDetails'
 import LoginPage from './components/auth/LoginPage'
@@ -19,34 +23,18 @@ import SignupPage from './components/auth/SignupPage'
 
 const App = () => {
   return (
-    <>
-      {/* <NaveBar/>
-      <NavCategorySlider/>
-      <Banner/>
-      <ShowProducts/>
-      <ShowProducts/>
-      <ShowProducts/>
-      <Footer/>
-      
-      <ProductPage/> 
-      <ProductList/>
-      <ProfilePage/>
-      <FavoritesPage/>
-      <CartPage/>
-      <CategorySlider/> */}
-      
     <BrowserRouter basename="/furniture-ecommerce">
-  <Routes>
-    <Route element={<Home />} path="/" />
-    <Route element={<Products />} path="/category/:catname/:catid" />
-    <Route element={<ProductDetails />} path="/product/:productid" />
-    <Route element={<LoginPage />} path="/login" />
-    <Route element={<SignupPage />} path="/signup" />
-    <Route element={<ProfilePage />} path="/profile" />
-  </Routes>
-</BrowserRouter>
-
-    </>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/category/:catname/:catid" element={<Products />} />
+        <Route path="/product/:productid" element={<ProductDetails />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignupPage />} />
+        <Route path="/profile" element={<ProfilePage />} />
+        <Route path="/favorites" element={<FavoritesPage />} />
+        <Route path="/cart" element={<CartPage />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
